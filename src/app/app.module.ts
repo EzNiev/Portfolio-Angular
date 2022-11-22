1a38e4cd74a15340c7ff6312c1972b6b7c3524ae
EzNiev <ezequiel.nieva1998@gmail.com>
2022-11-22 01:46:48 -0300

    Se agregan los selectores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from "./components/header/header.component";
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { IndexComponent } from './components/index/index.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { Page404Component } from './components/page404/page404.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         FooterComponent,
         HeaderComponent,
+        EducacionComponent,
+        ExperienciaComponent,
+        SkillsComponent,
+        IndexComponent,
+        LoginComponent,
+        LogoutComponent,
+        NavbarComponent,
+        Page404Component,
+        ProyectsComponent,
+        AboutMeComponent,
     ],
     imports: [
         BrowserModule,
