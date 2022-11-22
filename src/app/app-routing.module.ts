ef45584abf1995205c2bb1f87a48eed37bca61d4
EzNiev <ezequiel.nieva1998@gmail.com>
2022-11-22 02:03:56 -0300

    Carga de primeras rutas de routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './components/index/index.component';
+import { Page404Component } from './components/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:IndexComponent},
+  {path:'**', component:Page404Component},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
