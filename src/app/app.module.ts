c9eb3a12aab8a175a42268a86acfdb3cca594be6
EzNiev <ezequiel.nieva1998@gmail.com>
2022-11-24 04:45:23 -0300

    Empezando a crear servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from "./components/header/header.component";
@@ -37,6 +39,8 @@ import { BannerComponent } from './components/banner/banner.component';
     imports: [
         BrowserModule,
         AppRoutingModule,
+        FormsModule,
+        HttpClientModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
