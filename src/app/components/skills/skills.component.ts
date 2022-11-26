5b7033908746d98bbc75d0a1e87f361e8861a62b
EzNiev <ezequiel.nieva1998@gmail.com>
2022-11-26 02:57:11 -0300

    Se cargan secciones educacion y skills
    Se terminan de cargar en base de datos educacion y skills y las trabajo con angular para mostrarlas en cada componente

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscriber } from 'rxjs';
+import { DatosService } from 'src/app/servicios/datos.service';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css']
 })
-export class SkillsComponent {
 
+export class SkillsComponent implements OnInit {
+  skills: any = [];
+  constructor(private datos: DatosService) { }
+
+  ngOnInit(): void {
+    this.datos.getDatos().subscribe( data => {
+      this.skills = data.skills;
+    })
+  }
 }
