b541999f45adc7bb8e87d6cfea16668ae27925fd
EzNiev <ezequiel.nieva1998@gmail.com>
2023-01-17 20:41:29 -0300

    Update modal-aboutme.component.ts

--- src/app/modales/modal-aboutme/modal-aboutme/modal-aboutme.component.ts
+++ src/app/modales/modal-aboutme/modal-aboutme/modal-aboutme.component.ts
@@ -11,7 +11,7 @@ import { DatosezeService } from 'src/app/servicios/datoseze.service';
 })
 export class ModalAboutmeComponent implements OnInit {
   form: FormGroup;
-  datoseze: Datoseze=null;
+  datoseze: Datoseze=any[];
   constructor(private formBuilder: FormBuilder,
     private SDatoseze: DatosezeService,
     private activatedRoute: ActivatedRoute,
